c8e3990095d90a453a7f375d0d468fb71d9556e0
Sword-99 <success.cs.27@gmail.com>
2025-05-24 20:44:19 -0500

    Agregar main

--- src/main.py
+++ src/main.py
@@ -1,1 +1,19 @@
-print("hola mundo")
+# src/main.py
+
+import tkinter as tk
+from gui_main import EmergenCityGUI
+
+def main():
+    root = tk.Tk()
+    app = EmergenCityGUI(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
+
+
+# Primero, importamos la librería tkinter como tk.
+
+# Luego, importamos la clase EmergenCityGUI desde el módulo gui_main.
+
+# Definimos la función main()
